f253cbe4f6d4da66bd1041f78b3cf074a749a6ab
sukantadas1 <sukantadas.das82@gmail.com>
2023-12-01 14:35:37 +0530

    almost done

---
+++ file-handling.js
@@ -0,0 +1,15 @@
+const content =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, deserunt?";
+
+function downloadContent() {
+  const blob = new Blob([content], { type: "text/plain" });
+  console.log(blob);
+  const url = URL.createObjectURL(blob);
+  console.log(url);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "temp.txt";
+
+  link.click();
+}
